ecd7c01fe81c1d079faee0c3449b1498b342286e
JoonHyukPark <joonhyukpark@localhost>
2020-04-09 14:25:17 +0900

    Initial commit (Skeleton code)

---
+++ SoftwareEngineering/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import login.LoginFrame;
+
+public class Main {
+	public static LoginFrame loginFrame = new LoginFrame();
+	
+	public static void main(String[] args) {
+		loginFrame.setThis();
+		
+	}
+
+}
